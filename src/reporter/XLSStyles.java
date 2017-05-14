ae295f8cf8ac5f5a6b4e6202755f4040de3ae69c
Tomer <tomerza@gmail.com>
2017-05-14 13:32:28 +0300

    adding new cell styles

--- src/reporter/XLSStyles.java
+++ src/reporter/XLSStyles.java
@@ -3,17 +3,6 @@ package reporter;
 import org.apache.poi.ss.usermodel.*;
 
 public class XLSStyles {
-    public CellStyle allBorderCellStyle(CellStyle cell){
-        cell.setBorderBottom(BorderStyle.THICK);
-        cell.setBottomBorderColor(IndexedColors.BLACK.getIndex());
-        cell.setBorderLeft(BorderStyle.THICK);
-        cell.setLeftBorderColor(IndexedColors.BLACK.getIndex());
-        cell.setBorderRight(BorderStyle.THICK);
-        cell.setRightBorderColor(IndexedColors.BLACK.getIndex());
-        cell.setBorderTop(BorderStyle.THICK);
-        cell.setTopBorderColor(IndexedColors.BLACK.getIndex());
-        return cell;
-    }
 
     public CellStyle alignmentCellStyle(CellStyle cell){
         cell.setAlignment(HorizontalAlignment.CENTER);
@@ -30,6 +19,84 @@ public class XLSStyles {
     public CellStyle bottomBorderCellStyle(CellStyle cell){
         cell.setBorderBottom(BorderStyle.THICK);
         cell.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+        return cell;
+    }
+
+    public CellStyle rightBorderCellStyle(CellStyle cell){
+        cell.setBorderRight(BorderStyle.THICK);
+        cell.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        return cell;
+    }
+
+    public CellStyle topBorderCellStyle(CellStyle cell){
+        cell.setBorderTop(BorderStyle.THICK);
+        cell.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        return cell;
+    }
+
+    public CellStyle bottomOpenCellBorder(CellStyle cell){
+        cell = this.alignmentCellStyle(cell);
+        cell = this.leftBorderCellStyle(cell);
+        cell = this.rightBorderCellStyle(cell);
+        cell = this.topBorderCellStyle(cell);
+        return  cell;
+    }
+
+    public CellStyle topOpenCellBorder(CellStyle cell){
+        cell = this.alignmentCellStyle(cell);
+        cell = this.leftBorderCellStyle(cell);
+        cell = this.rightBorderCellStyle(cell);
+        cell = this.bottomBorderCellStyle(cell);
+        return  cell;
+    }
+
+    public CellStyle bottomTopOpenCellBorder(CellStyle cell){
+        cell = this.alignmentCellStyle(cell);
+        cell = this.leftBorderCellStyle(cell);
+        cell = this.rightBorderCellStyle(cell);
+        return  cell;
+    }
+
+    public CellStyle allBorderCellStyle(CellStyle cell){
+        cell.setBorderBottom(BorderStyle.THICK);
+        cell.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+        cell.setBorderLeft(BorderStyle.THICK);
+        cell.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        cell.setBorderRight(BorderStyle.THICK);
+        cell.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        cell.setBorderTop(BorderStyle.THICK);
+        cell.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        return cell;
+    }
+
+    public CellStyle colunm1BackgroundColor(CellStyle cell){
+        cell.setFillForegroundColor(IndexedColors.YELLOW.index);
+        cell.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        return cell;
+    }
+    public CellStyle colunm2BackgroundColor(CellStyle cell){
+        cell.setFillForegroundColor(IndexedColors.RED.index);
+        cell.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        return cell;
+    }
+    public CellStyle colunm3BackgroundColor(CellStyle cell){
+        cell.setFillForegroundColor(IndexedColors.LIGHT_GREEN.index);
+        cell.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        return cell;
+    }
+    public CellStyle colunm4BackgroundColor(CellStyle cell){
+        cell.setFillForegroundColor(IndexedColors.SKY_BLUE.index);
+        cell.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        return cell;
+    }
+    public CellStyle colunm5BackgroundColor(CellStyle cell){
+        cell.setFillForegroundColor(IndexedColors.ROYAL_BLUE.index);
+        cell.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        return cell;
+    }
+    public CellStyle colunm6BackgroundColor(CellStyle cell){
+        cell.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.getIndex());
+        cell.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         return cell;
     }
 }
