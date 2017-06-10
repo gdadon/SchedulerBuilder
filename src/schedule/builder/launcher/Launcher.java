9c3a839c0b4ce360eda9b2a05ffbdb272f1e88eb
gdadon <guydadon22@gmail.com>
2017-06-10 13:15:53 +0300

    new lanchuer

--- src/schedule/builder/launcher/Launcher.java
+++ src/schedule/builder/launcher/Launcher.java
@@ -1,11 +1,6 @@
 package schedule.builder.launcher;
 
-import objects.Scheduler;
-import reporter.ReporterScheduler;
-import schedule.builder.algorithm.LocalSearch;
-import schedule.builder.algorithm.Optimizer;
-import schedule.builder.algorithm.Ranking;
-import schedule.builder.scheduler.BaseSchedulerBuilder;
+import schedule.builder.database.DatabaseUtil;
 
 public class Launcher {
 
@@ -14,42 +9,62 @@ public class Launcher {
 //    check if DB exist, if no build it
 //        TODO need to add check if DB exist
 
-//        DatabaseUtil.createDB();
-//        DatabaseUtil.fillDB("NewReports");
-//
-//        System.out.println("=============================================");
-//        System.out.println("=== Data Base has been created and filled ===");
-//        System.out.println("=============================================");
+        DatabaseUtil.dropDataBase("sbdb");
 
+        DatabaseUtil.createDB();
+        DatabaseUtil.fillDB("Practice Reports");
+
+        System.out.println("=============================================");
+        System.out.println("=== Data Base has been created and filled ===");
+        System.out.println("=============================================");
+/*
         // build base scheduler
         BaseSchedulerBuilder build = new BaseSchedulerBuilder();
         Scheduler scheduler = build.buildBaseScheduler();
-        // save base scheduler
-        ReporterScheduler rs = new ReporterScheduler("output\\BaseScheduler", scheduler);
+        ReporterScheduler rs = null;
+//        // save base scheduler
+//        ReporterScheduler rs = new ReporterScheduler("output\\BaseWithPractices", scheduler);
+        SchedulerUtils.saveSchedulerToFile(scheduler, "BaseWithPractices");
         // apply local search on base scheduler and save
+//        Scheduler scheduler = SchedulerUtils.loadSchedulerFromFile("BaseWithPractices");
+//        ReporterScheduler rs = null;
+
         scheduler = LocalSearch.startLocalSearch(scheduler);
+        // check if there are still any conflicts
+
+        int conflicts = ConflictCounter.countConflict(scheduler);
+
+        if (conflicts > 0){
+            System.exit(1);
+        }
+        else{
+            System.out.println("Conflicts were resolved");
+        }
+
         System.out.println("Score after local search: " + Ranking.getOptimizationScore(scheduler));
         rs = new ReporterScheduler("output\\SchedulerPostLocalSearch", scheduler);
 
         scheduler = Optimizer.optimize(scheduler);
         System.out.println("Score after optimization: " + Ranking.getOptimizationScore(scheduler));
-        rs = new ReporterScheduler("output\\SchedulerPostLocalSearch", scheduler);
+        rs = new ReporterScheduler("output\\SchedulerPostOptimization", scheduler);
 
         boolean isScoreGood = false;
         int round = 0;
-        while(/*!isScoreGood &&*/ round < 10){
+        while(!isScoreGood && round < 10){
             build = new BaseSchedulerBuilder();
             scheduler = build.buildBaseScheduler();
             scheduler = LocalSearch.startLocalSearch(scheduler);
             scheduler = Optimizer.optimize(scheduler);
             int score = Ranking.getOptimizationScore(scheduler);
             System.out.println("Score in round " + round + ": "  + score);
-            if (score >= 95){
+//            if (score >= 95){
 //                isScoreGood = true;
                 System.out.println("Saving Scheduler");
+//                SchedulerUtils.saveSchedulerToFile(scheduler, "baseWithPractice"+ round + "score" + score);
                 rs = new ReporterScheduler("output\\SchedulerRound" + round + "score" + score, scheduler);
-            }
+//            }
             round ++;
         }
+        */
     }
 }
